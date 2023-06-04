b1f0dbf0727db67be340edb5cb4dd3d7f4c4dddb
XaN0037 <xannodirbek@gmail.com>
2023-06-04 10:38:47 +0500

    asdasd

--- api/admin.py
+++ api/admin.py
@@ -4,7 +4,7 @@ from api.models.news import *
 from api.models.doctors import *
 from api.models.contact import *
 
-
 admin.site.register(Doctor)
 admin.site.register(Contact)
 admin.site.register(New)
+
--- dashboard/v1/patient/views.py
+++ dashboard/v1/patient/views.py
@@ -2,23 +2,22 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from api.models import Doctor
-from api.v1.doctors.serializer import DoctorSerializer
-from base.formats import doctor_format, patient_format_all, patient_format_one
+from base.formats import  patient_format_all, patient_format_one
 from base.errors import *
 from dashboard.models import Patient
+from dashboard.v1.patient.serializer import PatientSerializer
 
 
 class PatientViews(GenericAPIView):
-    serializer_class = DoctorSerializer
+    serializer_class = PatientSerializer
     permission_classes = (AllowAny,)
 
-    # def post(self, requests, *args, **kwargs):
-    #     serializer = self.get_serializer(data=requests.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     root = serializer.save()
-    #
-    #     return Response(doctor_format(root))
+    def post(self, requests, *args, **kwargs):
+        serializer = self.get_serializer(data=requests.data)
+        serializer.is_valid(raise_exception=True)
+        root = serializer.save()
+
+        return Response(patient_format_all(root))
 
     def get(self, requests, *args, **kwargs):
         data = requests.query_params
@@ -36,23 +35,24 @@ class PatientViews(GenericAPIView):
 
 
 
-    # def put(self, requests, *args, **kwargs):
-    #     data = requests.query_params
-    #     doctor = ''
-    #     try:
-    #         doctor = Doctor.objects.get(pk=requests.query_params('pk'))
-    #         serializer = self.get_serializer(data=data, instance=doctor, partial=True)
-    #         serializer.is_valid(raise_exception=True)
-    #         root = serializer.save()
-    #         return Response(doctor_format(root))
-    #
-    #     except:
-    #         return Response(MESSAGE["DoctorNotFound"])
-    #
-    # def delete(self, requests, *args, **kwargs):
-    #     try:
-    #         root = Doctor.objects.get(pk=requests.query_params('pk'))
-    #         root.delete()
-    #         return Response(MESSAGE[f"Doctordelet"])
-    #     except:
-    #         return Response(MESSAGE["Doctordeleteerror"])
+    def put(self, requests, *args, **kwargs):
+        data = requests.query_params
+        patient = ''
+        try:
+            patient = Patient.objects.get(pk=data.get('pk'))
+            print(patient_format_all(patient))
+            serializer = self.get_serializer(data=data, instance=patient, partial=True)
+            serializer.is_valid(raise_exception=True)
+            root = serializer.save()
+            return Response(patient_format_all(root))
+
+        except:
+            return Response(MESSAGE["DoctorNotFound"])
+
+    def delete(self, requests, *args, **kwargs):
+        try:
+            root = Patient.objects.get(pk=requests.query_params('pk'))
+            root.delete()
+            return Response(MESSAGE[f"Doctordelet"])
+        except:
+            return Response(MESSAGE["Doctordeleteerror"])
